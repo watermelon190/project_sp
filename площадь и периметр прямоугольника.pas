307516cb855688eb3f79e820ea25875b89b851a5
watermelon190 <161962734+watermelon190@users.noreply.github.com>
2024-03-02 19:51:37 +0300

    project

---
+++ площадь и периметр прямоугольника.pas
@@ -0,0 +1,15 @@
+﻿program ploshad i perimetr pramoygolnika;
+
+var
+  a, b, s, p: real;
+
+begin
+  writeln('введите первое значение');
+  read(a);
+  writeln('введите второе значение');
+  read(b);
+  s := a * b;
+  p := 2 * a + 2 * b;
+  writeln('площадь  = ', s);
+  writeln('периметр = ', p);
+end.
